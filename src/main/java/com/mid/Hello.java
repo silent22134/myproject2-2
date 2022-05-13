f2e20f3faa8d9d439dcb1e68c1c134e7883b3b24
silent22134 <93965960+silent22134@users.noreply.github.com>
2022-05-13 16:17:28 +0800

    Date tester

--- src/main/java/com/mid/Hello.java
+++ src/main/java/com/mid/Hello.java
@@ -5,6 +5,6 @@ import com.mid.score.Student;
 public class Hello {
     public static void main(String[] args) {
         Student stu = new Student();
-        System.out.println(stu.name);
+        System.out.println(stu);
     }
 }
---
+++ src/main/java/com/mid/parking/Runner.java
@@ -0,0 +1,35 @@
+package com.mid.parking;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Runner {
+    public static void main(String[] args) {
+        String id = "ABC-1234";
+        String entertime = "08:00";
+        String exittime = "11:16";
+        SimpleDateFormat sdf =new SimpleDateFormat("HH:mm");
+        Date date =null;
+        try{
+            date =sdf.parse(entertime);
+        }catch (Exception e){
+            System.out.println("Wrong Format");
+        }
+        System.out.println(date);
+        System.out.println(date.getTime());
+        long ms = 8*60*60*1000;
+        Date date2 = null;
+        try{
+            date2 = sdf.parse(exittime);
+            System.out.println(date2);
+            System.out.println(date2.getTime());
+            int mins = (int)(date2.getTime()-date.getTime())/1000/60;
+            System.out.println(mins);
+            int fee = 30*(mins/60);
+            System.out.println("Fee :"+fee);
+
+        }catch (Exception e){
+            System.out.println("Wrong Format");
+        }
+    }
+}
